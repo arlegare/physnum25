1e1646125fd99b25777e6d561a4b73b89689a86d
arleg <arthur.legare.1@ulaval.ca>
2025-04-23 11:12:08 -0400

    simul_ising avec methode au choix, encore bugs

--- simul_ising.py
+++ simul_ising.py
@@ -8,92 +8,25 @@ import os
 import sys
 
 ## Permettre le choix entre méthode avec boucles for accélérées par Numba, méthode avec FFT
-## Permettra de faire des tests de rapidité!
+## Permettre de faire des tests de rapidité!
+## Simuler en batch. Parallélisation? -> Chiant à mettre sur windows, donc devra rester sur la tour.
+## Quantification de l'aire de la courbe pour constante? 
+## Lien avec dimension fractale?
 
-@nb.njit(nopython=True)
-def microstate_energy(lattice, h, size):
-
+def convolution_2d_fft(lattice, kernel):
     """
-    Calcule l'énergie totale d'un micro-état donné (lattice : configuration de spins; h : composante Z du champ magnétique).
-
-    Entrée :
-        lattice (np.ndarray): Configuration de spins.
-        h (float): H/J, où  Composante Z du champ magnétique.
-        betaJ (float): Ratio de la constante de couplage J sur k_BT (positif pour ferromagnétisme, négatif pour antiferromagnétisme).
-        size (int): Taille de la grille.
-
-    Sortie :
-        float: Énergie totale du micro-état.
-    """
-
-    energie_totale = 0.0
-
-    for i in range(size):
-        for j in range(size):
-            spin = lattice[i, j]
-            # Conditions périodiques (Pac-Man)
-            voisinage = (
-                lattice[(i + 1) % size, j] +  # Voisin du bas
-                lattice[(i - 1) % size, j] +  # Voisin du haut
-                lattice[i, (j + 1) % size] +  # Voisin de droite
-                lattice[i, (j - 1) % size]    # Voisin de gauche
-            )
-            # Contribution de l'énergie du spin
-            energie_totale -=  spin * voisinage  # Interaction avec les voisins
-
-            # Contribution de l'énergie du champ magnétique
-            energie_totale -= h * spin  # Signe (-) car si spin est dans la même direction que le champ, l'énergie est minimisée.
-
-    return energie_totale
-
-@nb.njit(nopython=True)
-def convolution_2d_periodic(lattice, kernel):
-        """
-        Effectue une convolution 2D avec conditions aux limites périodiques.
-
-        Entrée :
-            lattice (np.ndarray): Grille de spins.
-            kernel (np.ndarray): Noyau de convolution.
-
-        Sortie :
-            np.ndarray: Résultat de la convolution.
-        """
-
-        N, M = lattice.shape
-        kh, kw = kernel.shape
-        kh2 = kh // 2
-        kw2 = kw // 2
-
-        result = np.zeros_like(lattice)
-
-        for i in range(N):
-            for j in range(M):
-                acc = 0.0
-                for u in range(-kh2, kh2 + 1):
-                    for v in range(-kw2, kw2 + 1):
-                        ni = (i + u) % N
-                        nj = (j + v) % M
-                        acc += lattice[ni, nj] * kernel[u + kh2, v + kw2]
-                result[i, j] = acc
-
-        return result
-
-# La convolution revient à faire la somme sur les s_j, où j correspond aux plus proches voisins (du mask).
-def convolution_2d_avec_fft(lattice, mask):
-    """
-    Effectue une convolution 2D en exploitant la FFT.
-    Forme compatible avec une accélération par Numba.
+    Effectue une convolution 2D en exploitant la FFT. Notion cool de PM2!
     """
     if lattice.shape[0] % 2 == 0:
-        raise ValueError("La dimension de la grille doit être impaire.")
+        raise ValueError("La dimension de la grille doit être impaire.") # Petite contrainte tannante par contre.
     
     # On effectue la transformée de Fourier des deux matrices.
     lattice_fourier = np.fft.fft2(lattice)
     #mask_fourier = np.fft.fft2(np.flipud(np.fliplr(mask))) 
-    mask_fourier = np.fft.fft2(np.fft.ifftshift(mask), s=lattice.shape)
+    kernel_fourier = np.fft.fft2(np.fft.ifftshift(kernel), s=lattice.shape)
 
    # Multiplication dans le domaine fréquentiel
-    cc_fourier = lattice_fourier * mask_fourier
+    cc_fourier = lattice_fourier * kernel_fourier
 
     # Transformée inverse pour revenir dans l'espace réel
     cc = np.real(np.fft.ifft2(cc_fourier))
@@ -109,35 +42,99 @@ def convolution_2d_avec_fft(lattice, mask):
 
     return cc
 
-@nb.njit(nopython=True)
-def interaction_energy(lattice, row, col, h, size):
+#@numba.njit
+def convolution_2d_mitaine(lattice, kernel):
     """
-    Calcule l'énergie d'interaction pour un spin donné.
-
-    Entrée  :
-        lattice (np.ndarray): Grille de spins.
-        row (int): Indice de la ligne du spin.
-        col (int): Indice de la colonne du spin.
-        h (float): Champ magnétique externe.
-        betaJ (float): Ratio de la constante de couplage J sur k_BT (positif pour ferromagnétisme, négatif pour antiferromagnétisme).
-        size (int): Taille de la grille.
-
-    Sortie :
-        float: Énergie d'interaction pour le spin donné.
+    Effectue une convolution 2D.
+    Forme compatible avec une accélération par Numba.
     """
+    N, M = lattice.shape
+    kh, kw = kernel.shape
+    kh2 = kh // 2
+    kw2 = kw // 2
 
-    spin = lattice[row, col]
-    # Conditions périodiques pour les voisins
-    voisinage = (
-        lattice[(row + 1) % size, col] +
-        lattice[(row - 1) % size, col] +
-        lattice[row, (col + 1) % size] +
-        lattice[row, (col - 1) % size]
-    )
-    # Énergie due au champ magnétique et aux interactions avec les voisins
-    return -h * spin - spin * voisinage
+    result = np.zeros_like(lattice)
 
-@nb.njit(nopython=True)
+    for i in range(N):
+        for j in range(M):
+            acc = 0.0
+            for u in range(-kh2, kh2 + 1):
+                for v in range(-kw2, kw2 + 1):
+                    ni = (i + u) % N
+                    nj = (j + v) % M
+                    acc += lattice[ni, nj] * kernel[u + kh2, v + kw2]
+            result[i, j] = acc
+
+    return result
+
+def extended_lattice(lattice): 
+    """
+    Applique les CF périodiques (Pac-Man) à la grille 2D.
+    L'astuce repose sur l'ajout de bordures de 1 ligne/colonne autour de la grille. 
+    La dimension effective de la grille est donc de (N+2) x (N+2). 
+    """
+    n = lattice.shape[0] # Taille de la grille
+
+    lattice_extended = np.concatenate([lattice, lattice, lattice], axis=1)  # On applique les CF à gauche et à droite
+    lattice_extended = np.concatenate([lattice_extended, lattice_extended, lattice_extended], axis=0)  # On applique les CF en haut et en bas.
+
+    return lattice_extended[n-1:2*n+1, n-1:2*n+1] 
+
+def correlation_energy(lattice, method="scipy"): 
+
+    if method == "scipy":
+
+        mask = sc.generate_binary_structure(2,1)  # Matrice 2D avec True seulement aux voisins plus proche (connectivité=1)
+        mask[1,1] = False  # On veut pas compter le spin lui même dans la somme
+
+        # On applique les conditions frontières périodiques avec l'argument wrap. 
+        # La convolution revient à faire la somme sur les s_j en prenant compte du fait que j correspond aux plus proches voisins
+        energy_array = -lattice * sc.convolve(lattice, mask, mode='wrap')  
+
+        return np.sum(energy_array)
+    
+    if method == "fft":
+
+        mask = np.ones((3,3), dtype=int)  # Matrice 2D avec 1 seulement aux voisins plus proche (connectivité=1).
+        mask[1,1] = 0  # Le spin est lui-même exclu de la somme.
+
+        # Utilisation de la fonction convolution_2d_avec_fft pour calculer l'énergie des interactions.
+        grille_etendue = extended_lattice(lattice)  # On applique les CF périodiques (Pac-Man)
+        energy_array = -lattice * convolution_2d_fft(grille_etendue, mask)[1:lattice.shape[0]+1,1:lattice.shape[0]+1]
+
+        return np.sum(energy_array)
+    
+    if method == "numba":
+        mask = np.ones((3,3), dtype=int)  # Matrice 2D avec 1 seulement aux voisins plus proche (connectivité=1).
+        mask[1,1] = 0  # Le spin est lui-même exclu de la somme.
+
+        #grille_etendue = extended_lattice(lattice)  # On applique les CF périodiques (Pac-Man)
+        energy_array = -lattice * convolution_2d_mitaine(lattice, mask)#[1:lattice.shape[0]+1,1:lattice.shape[0]+1]
+
+        return np.sum(energy_array)
+
+    
+def microstate_energy(lattice, h, method="scipy"):
+    """
+    Calcule l'énergie totale d'un micro-état donné (lattice : configuration de spins; h : composante Z du champ magnétique).
+
+    On doit tenir compte de deux contributions : 
+        1) les voisins immédiats;
+        2) le champ magnétique externe.
+    """
+    N = lattice.shape[0]
+    energie_mag = 0.0
+
+    # Contribution du champ magnétique externe
+    energie_mag -= h * np.sum(lattice)  # Utilisation de la somme vectorisée pour accélérer le calcul.
+
+    # Contribution des interactions entre voisins
+    energie_corr = correlation_energy(lattice, method=method)
+
+    return energie_mag + energie_corr
+
+
+#@nb.njit(nopython=True)
 def find_equilibrium(lattice, n_iter, betaJ, h, size, convol="numba"):
     """
     Trouve l'état d'équilibre en utilisant l'algorithme de Metropolis.
@@ -163,8 +160,11 @@ def find_equilibrium(lattice, n_iter, betaJ, h, size, convol="numba"):
         
         # Terme dû au champ + terme de corrélation avec conditions frontières périodiques.
         # On calcule seulement l'énergie du spin concerné puisque les autres ne changent pas.
-        E_i = interaction_energy(lattice, row, col, h, size, convol) # Avant le flip.
-        E_f = interaction_energy(new_lattice, row, col, h, size, convol) # Après le flip.
+        E_i = microstate_energy(lattice, h, convol)
+        E_f = microstate_energy(new_lattice, h, convol)
+
+        #E_i = interaction_energy(lattice, row, col, h, size, convol) # Avant le flip.
+        #E_f = interaction_energy(new_lattice, row, col, h, size, convol) # Après le flip.
 
         DeltaE = E_f - E_i  # Variation d'énergie
 
@@ -199,9 +199,10 @@ class Metropolis():
         self.n_iter = n_iter  # Nb d'itérations/steps pour la simulation. Par ex. choisir assez long pour atteindre l'équilibre pour une valeur de (T,B).
         self.size = lattice_size  # Dimension de la grille (carrée) de spins. 
         self.h = magnetic_field  # Champ magnétique externe.
-        self.betaJ = betaJ
+        #self.temperature = temperature # Température
+        self.betaJ = betaJ # betaJ
         self.up_perc = pourcentage_up  # Pourcentage de spins orienté up dans la grille initiale (entre 0 et 1)
-        self.convol = convol
+        self.convol = convol # Méthode de convolution. Comparer les méthdodes devient un élément de discussion intéressant.
 
         if previous_lattice is not None:
             self.lattice = previous_lattice
